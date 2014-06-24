25419ccb213d72c6779a5ba9e30783871c648fc2
ChrisBarthol <Chris.Barthol@gmail.com>
2014-06-24 13:27:15 -0400

    Fixed problem 20

--- problem20.rb
+++ problem20.rb
@@ -2,4 +2,10 @@
 
  #   Find the sum of the digits in the number 100!
 
-puts 100.downto(1).inject(:+) #5050
+factorial_100 = 100.downto(1).inject(:*)
+
+def get_digits(n)
+	Math.log10(n).floor.downto(0).map { |i| (n /10**i) %10 }
+end
+
+puts get_digits(factorial_100).inject(:+)
